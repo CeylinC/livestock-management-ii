f426be8663808bc7a09bee06d7c8d0a2a6987d5f
CeylinC <caltepeceylin@gmail.com>
2025-01-14 03:18:56 +0300

    feat: edit dashboard

---
+++ packages/shared/utils/toReadableSalesCategories.ts
@@ -0,0 +1,9 @@
+export const toReadableSalesCategories = {
+    ["ANIMAL_PRODUCT"]: "Hayvansal Ürün",
+    ["ANIMAL"]: "Hayvan",
+    ["FERTILIZER"]: "Gübre",
+    ["AGRICULTURE"]: "Tarım Ürünü",
+    ["FEED"]: "Yem",
+    ["MEDICINE"]: "İlaç",
+    ["EQUIPMENT"]: "Ekipman",
+};
